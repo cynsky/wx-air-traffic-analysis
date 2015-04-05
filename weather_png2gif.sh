c99e0f621a7865f4c7a2729efa8231d859cd573d
Kenneth Kuhn <kenneth.kuhn@gmail.com>
2015-04-05 11:05:36 -0700

    clustering typo

--- weather_png2gif.sh
+++ weather_png2gif.sh
@@ -7,7 +7,7 @@
 
 echo Turning weather maps into gifs
 
-for i in {2010..2014}
+for i in {2010..2013}
 do
 	for j in {1..12}
 	do
@@ -15,31 +15,31 @@ do
 		do
 			if [ ${j} -lt 10 ]; then
 				if [ ${k} -lt 10 ]; then
-					# gm convert -delay 40 /Volumes/NASA_data_copy/output_other/nyc_wx_gifs/Wx_${i}_0${j}_0${k}*.png /Volumes/NASA_data_copy/output_other/nyc_wx_gifs/Wx_${i}_0${j}_0${k}.gif
-					if [ -e /Volumes/NASA_data_copy/output_other/nyc_wx_gifs/RUC_${i}_0${j}_0${k}_00.png ]; then
-						gm convert -delay 40 /Volumes/NASA_data_copy/output_other/nyc_wx_gifs/RUC_${i}_0${j}_0${k}*.png /Volumes/NASA_data_copy/output_other/nyc_wx_gifs/RUC_${i}_0${j}_0${k}.gif
-					fi
-				else
-					# if [ -e /Volumes/NASA_data_copy/output_other/nyc_wx_gifs/Wx_${i}_0${j}_${k}_00.png ]; then
-						# gm convert -delay 40 /Volumes/NASA_data_copy/output_other/nyc_wx_gifs/Wx_${i}_0${j}_${k}*.png /Volumes/NASA_data_copy/output_other/nyc_wx_gifs/Wx_${i}_0${j}_${k}.gif
+					gm convert -delay 40 /Volumes/NASA_data_copy/output_other/nyc_wx_gifs/Wx_${i}_0${j}_0${k}*.png /Volumes/NASA_data_copy/output_other/nyc_wx_gifs/Wx_${i}_0${j}_0${k}.gif
+					# if [ -e /Volumes/NASA_data_copy/output_other/nyc_wx_gifs/RUC_${i}_0${j}_0${k}_00.png ]; then
+					# 	gm convert -delay 40 /Volumes/NASA_data_copy/output_other/nyc_wx_gifs/RUC_${i}_0${j}_0${k}*.png /Volumes/NASA_data_copy/output_other/nyc_wx_gifs/RUC_${i}_0${j}_0${k}.gif
 					# fi
-					if [ -e /Volumes/NASA_data_copy/output_other/nyc_wx_gifs/RUC_${i}_0${j}_${k}_00.png ]; then
-						gm convert -delay 40 /Volumes/NASA_data_copy/output_other/nyc_wx_gifs/RUC_${i}_0${j}_${k}*.png /Volumes/NASA_data_copy/output_other/nyc_wx_gifs/RUC_${i}_0${j}_${k}.gif
+				else
+					if [ -e /Volumes/NASA_data_copy/output_other/nyc_wx_gifs/Wx_${i}_0${j}_${k}_00.png ]; then
+						gm convert -delay 40 /Volumes/NASA_data_copy/output_other/nyc_wx_gifs/Wx_${i}_0${j}_${k}*.png /Volumes/NASA_data_copy/output_other/nyc_wx_gifs/Wx_${i}_0${j}_${k}.gif
 					fi
+					# if [ -e /Volumes/NASA_data_copy/output_other/nyc_wx_gifs/RUC_${i}_0${j}_${k}_00.png ]; then
+					# 	gm convert -delay 40 /Volumes/NASA_data_copy/output_other/nyc_wx_gifs/RUC_${i}_0${j}_${k}*.png /Volumes/NASA_data_copy/output_other/nyc_wx_gifs/RUC_${i}_0${j}_${k}.gif
+					# fi
 				fi
 			else
 				if [ ${k} -lt 10 ]; then
-					# gm convert -delay 40 /Volumes/NASA_data_copy/output_other/nyc_wx_gifs/Wx_${i}_${j}_0${k}*.png /Volumes/NASA_data_copy/output_other/nyc_wx_gifs/Wx_${i}_${j}_0${k}.gif
-					if [ -e /Volumes/NASA_data_copy/output_other/nyc_wx_gifs/RUC_${i}_${j}_0${k}_00.png ]; then
-						gm convert -delay 40 /Volumes/NASA_data_copy/output_other/nyc_wx_gifs/RUC_${i}_${j}_0${k}*.png /Volumes/NASA_data_copy/output_other/nyc_wx_gifs/RUC_${i}_${j}_0${k}.gif
-					fi
-				else
-					# if [ -e /Volumes/NASA_data_copy/output_other/nyc_wx_gifs/Wx_${i}_${j}_${k}_00.png ]; then
-						# gm convert -delay 40 /Volumes/NASA_data_copy/output_other/nyc_wx_gifs/Wx_${i}_${j}_${k}*.png /Volumes/NASA_data_copy/output_other/nyc_wx_gifs/Wx_${i}_${j}_${k}.gif
+					gm convert -delay 40 /Volumes/NASA_data_copy/output_other/nyc_wx_gifs/Wx_${i}_${j}_0${k}*.png /Volumes/NASA_data_copy/output_other/nyc_wx_gifs/Wx_${i}_${j}_0${k}.gif
+					# if [ -e /Volumes/NASA_data_copy/output_other/nyc_wx_gifs/RUC_${i}_${j}_0${k}_00.png ]; then
+					# 	gm convert -delay 40 /Volumes/NASA_data_copy/output_other/nyc_wx_gifs/RUC_${i}_${j}_0${k}*.png /Volumes/NASA_data_copy/output_other/nyc_wx_gifs/RUC_${i}_${j}_0${k}.gif
 					# fi
-					if [ -e /Volumes/NASA_data_copy/output_other/nyc_wx_gifs/RUC_${i}_${j}_${k}_00.png ]; then
-						gm convert -delay 40 /Volumes/NASA_data_copy/output_other/nyc_wx_gifs/RUC_${i}_${j}_${k}*.png /Volumes/NASA_data_copy/output_other/nyc_wx_gifs/RUC_${i}_${j}_${k}.gif
+				else
+					if [ -e /Volumes/NASA_data_copy/output_other/nyc_wx_gifs/Wx_${i}_${j}_${k}_00.png ]; then
+						gm convert -delay 40 /Volumes/NASA_data_copy/output_other/nyc_wx_gifs/Wx_${i}_${j}_${k}*.png /Volumes/NASA_data_copy/output_other/nyc_wx_gifs/Wx_${i}_${j}_${k}.gif
 					fi
+					# if [ -e /Volumes/NASA_data_copy/output_other/nyc_wx_gifs/RUC_${i}_${j}_${k}_00.png ]; then
+					# 	gm convert -delay 40 /Volumes/NASA_data_copy/output_other/nyc_wx_gifs/RUC_${i}_${j}_${k}*.png /Volumes/NASA_data_copy/output_other/nyc_wx_gifs/RUC_${i}_${j}_${k}.gif
+					# fi
 				fi
 			fi
 		done
